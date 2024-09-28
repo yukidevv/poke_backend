8f1fd91323591fa7352e1d2c9c27ed3708dff1c9
yukidevv <nomad048@gmail.com>
2024-09-28 16:09:23 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI
+import requests
+
+def main():
+  base_url = 'https://pokeapi.co/api/v2/'
+  add_url = 'pokemon/'
+  id = '1'
+
+  response = requests.get(base_url + add_url + id)
+  response = response.json()
+  print(response['height'])
+  print(response['weight'])
+  result = get_pokemon_data_japanese(response['name'])
+  if isinstance(result, tuple):
+    japanese_name, description = result
+    print(japanese_name)
+    print(description)
+
+def get_pokemon_data_japanese(pokemon_name):
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+      data = response.json()
+
+      #Japanese name
+      japanese_name = None
+      for name in data['names']:
+        if name['language']['name'] == 'ja-Hrkt':
+          japanese_name = name['name']
+          break
+      #Description
+      japanese_description = None
+      for entry in data['flavor_text_entries']:
+        if entry['language']['name'] == 'ja-Hrkt':
+          japanese_description = entry['flavor_text'].replace('\n', ' ').replace('\f', ' ')
+          break
+  
+      if japanese_name and japanese_description:
+        return japanese_name, japanese_description
+
+if __name__ == '__main__':
+  main()
+
+"""
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+  if q:
+    return {"item_id": item_id, "q": q}
+  return {"item_id": item_id}
+"""
